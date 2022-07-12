103078f4837ed00c1af9b8d7797b576108327970
Ion <92377687+MJonny4@users.noreply.github.com>
2022-07-12 17:43:43 +0200

    Ximuntion.com v0.1

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ximuntion</title>
+  
+  <link rel="shortcut icon" href="https://static.vecteezy.com/system/resources/thumbnails/001/192/577/small/x.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="./styles/style.css">
+
+  <script src="https://kit.fontawesome.com/fbccc5a75e.js" crossorigin="anonymous"></script>
+  <script src="https://www.w3schools.com/lib/w3.js"></script>
+</head>
+<body> 
+  <?php include './templates/navbar.php'; ?>
+  <?php include './templates/body.php'; ?>
+</body>
+</html>
---
+++ templates/body.php
@@ -0,0 +1,106 @@
+<div id="home" class="w3-content"> 
+  <img src="./images/1.jpg"  alt="day"   class="w3-image slides w3-card-4 w3-round">
+  <img src="./images/3.jpg"  alt="noon" class="w3-image slides w3-card-4 w3-round">
+  <img src="./images/2.jpg"  alt="night" class="w3-image slides w3-card-4 w3-round">
+  <script>
+    w3.slideshow(".slides", 5000);
+  </script>
+
+  <div class="w3-container w3-center w3-padding-32">
+    <h3 class="w3-wide">Welcome to Ximuntion</h3>
+    <p class="w3-opacity">A webpage made by Ion Munteanu</p>
+  </div>
+
+  <div id="about" class="w3-container w3-padding-32 w3-card-4 w3-round">
+    <div class="w3-row">
+      <div class="w3-col m6 w3-padding-large">
+        <h1 class="w3-center">About Me</h1>
+        <h1 class="w3-center"><i class="fa-solid fa-user-astronaut fa-4x"></i></h1>
+      </div>
+      <div class="w3-col m6 w3-padding-large">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea iusto inventore animi aspernatur at sit doloremque 
+          quia reiciendis consectetur, quisquam quas voluptatum harum nisi in accusamus neque earum incidunt autem.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id asperiores eligendi dicta voluptatibus eum qui veniam, 
+          nulla ad quos consequuntur fugiat animi repellendus, vitae possimus dolor consectetur cupiditate omnis? Sint!
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div id="features" class="w3-container w3-padding-32 w3-card-4 w3-round">
+    <h1 class="w3-center">Features</h1>
+    <div class="w3-row w3-center">
+      <div class="w3-col m3 w3-padding-large">
+        <i class="fa-solid fa-desktop fa-5x"></i>
+        <h3>Responsive</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+      </div>
+      <div class="w3-col m3 w3-padding-large">
+        <i class="fa-solid fa-pencil-ruler fa-5x"></i>
+        <h3>Design</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+      </div>
+      <div class="w3-col m3 w3-padding-large">
+        <i class="fa-solid fa-cogs fa-5x"></i>
+        <h3>Easy to use</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+      </div>
+      <div class="w3-col m3 w3-padding-large">
+      <i class="fa-solid fa-face-smile fa-5x"></i>
+        <h3>Good</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.</p>
+      </div>
+    </div>
+  </div>
+
+    <div id="contact" class="w3-container w3-padding-32 w3-card-4 w3-round w3-white">
+      <h1 class="w3-center">Contact</h1>
+      <p class="w3-center"><em>I'd love to hear from you!</em></p>
+      <div class="w3-row w3-padding-32">
+        <div class="w3-col m6 w3-large w3-margin-bottom">
+          <p class="w3-justify" style="padding-left: 80px;">
+          <i class="fa-solid fa-location-dot fa-1x"></i></i> Chicago, US<br>
+          <i class="fa-solid fa-phone fa-1x"></i> Phone: +00 151515<br>
+          <i class="fa-solid fa-envelope fa-1x"></i> Email: mail@mail.com<br>
+          </p>
+        </div>
+        <div class="w3-col m6">
+          <form action="contact.php" method="POST">
+            <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
+              <div class="w3-half">
+                <input class="w3-input w3-border" type="text" placeholder="Name" required name="name">
+              </div>
+              <div class="w3-half">
+                <input class="w3-input w3-border" type="text" placeholder="Email" required name="email">
+              </div>
+            </div>
+            <input class="w3-input w3-border" type="text" placeholder="Message" required name="message">
+            <button class="w3-btn w3-black w3-section w3-right" type="submit">SEND</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
+  <div id="bye" class="w3-center w3-padding-12">
+    <img src="http://pngimg.com/uploads/goodbye/goodbye_PNG22.png"  alt="night" class="w3-image slides w3-round">
+  </div>
+
+  <div id="footer" class="w3-center w3-padding-32">
+      <span class="w3-text-grey">&copy; 2022 Ximuntion</span>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
